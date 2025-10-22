62b30d07ec5cd563c1542bb83cc54ac7d99995fb
dxL1nus <linusldx@gmail.com>
2025-10-22 21:12:05 +0200

    Fixed API bug by implemented our very own journal/codecheck_api/{ourApiRoute} (https://github.com/codecheckers/ojs-codecheck/issues/48)

--- controllers/CodecheckAPIHandler.php
+++ controllers/CodecheckAPIHandler.php
@@ -12,10 +12,31 @@ use PKP\security\authorization\RoleBasedHandlerOperationPolicy;
 use PKP\security\Role;
 
 // header for AJAX calls
+define('INDEX_FILE_STARTED', true);
 header('Content-Type: application/json');
 
 class CodecheckAPIHandler
 {
+    private $apiRoute;
+
+    function __construct(string $route)
+    {
+        $this->apiRoute = $route;
+    }
+
+    public function serveApiRoute()
+    {
+        switch ($this->apiRoute) {
+            case 'getVenueData':
+                $this->getVenueData();
+                break;
+            
+            default:
+                error_log("[CODECHECK API Hanlder] -> at Route: $this->apiRoute -> 404: Requested unknown API Route");
+                break;
+        }
+    }
+
     public static function getVenueData()
     {
         $codecheckVenueTypes = new CodecheckVenueTypes();
@@ -28,5 +49,6 @@ class CodecheckAPIHandler
         ];
 
         echo json_encode($result);
+        exit();
     }
 }
