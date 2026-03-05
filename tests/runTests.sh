730f7d86862f15a5a581a00651ae481b63bc8208
dxL1nus <linusldx@gmail.com>
2026-03-05 11:48:15 +0100

    Removed unnecessary comments in runTests.sh #79

--- tests/runTests.sh
+++ tests/runTests.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env sh
 
-# Standard-Command
 DEFAULT_CMD="php ../../../../lib/pkp/lib/vendor/phpunit/phpunit/phpunit --configuration phpunit.xml ."
 
-# Command mit Coverage
 COVERAGE_CMD="php ../../../../lib/pkp/lib/vendor/phpunit/phpunit/phpunit --configuration phpunit_with_coverage.xml ."
 
-# Kein Parameter -> Standard
+# No Parameter -> Standard
 if [ -z "$1" ]; then
     exec $DEFAULT_CMD
 fi
 
-# Parameter prüfen
 case "$1" in
     --coverage-report=true)
         exec $COVERAGE_CMD
@@ -20,7 +17,7 @@ case "$1" in
         exec $DEFAULT_CMD
         ;;
     *)
-        # unbekannter Parameter -> Standard
+        # unknown Parameter -> Standard
         exec $DEFAULT_CMD
         ;;
 esac
